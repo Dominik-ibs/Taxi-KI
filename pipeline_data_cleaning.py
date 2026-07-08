57c267d0d5e23fe02f256118a9afbc42cf9c5462
00090854 <hannes.luebbers@enercon.de>
2026-07-08 16:45:23 +0200

    Added a new File for functions creating scaler und encoder

--- pipeline_data_cleaning.py
+++ pipeline_data_cleaning.py
@@ -1,11 +1,8 @@
-from pathlib import Path
-
-import joblib
 import numpy as np
 import pandas as pd
 import sqlite3
 import os
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+import creating_encoder_scaler
 
 # ==========================
 # Konfiguration
@@ -62,75 +59,10 @@ rows_after = 0
 mode = "replace"
 
 # ==========================
-# Speichern des Min Max Scalers
+# Creating encoder und scaler
 # ==========================
-
-SCALER_DIR = Path("scalers")
-SCALER_DIR.mkdir(parents=True, exist_ok=True)
-
-SCALER_FILE = SCALER_DIR / "minmax_scaler.pkl"
-
-SCALER_COLUMNS = ["trip_distance", "Duration"]
-
-FIXED_MIN_VALUES = {
-    "trip_distance": 0,
-    "Duration": 10
-}
-
-FIXED_MAX_VALUES = {
-    "trip_distance": 100,
-    "Duration": 10800
-}
-
-
-if os.path.exists(SCALER_FILE):
-    scaler = joblib.load(SCALER_FILE)
-    print("Scaler geladen.")
-else:
-    scaler = MinMaxScaler()
-
-    fixed_scaler_data = pd.DataFrame([
-        FIXED_MIN_VALUES,
-        FIXED_MAX_VALUES
-    ])
-
-    scaler.fit(fixed_scaler_data[SCALER_COLUMNS])
-
-    joblib.dump(scaler, SCALER_FILE)
-    print("Scaler mit festen Min-/Max-Werten erstellt und gespeichert.")
-
-
-# Encoder laden oder erstellen
-
-ENCODER_COLUMNS = ["day_of_week", "month"]
-ENCODER_DIR = Path("encoders")
-ENCODER_DIR.mkdir(parents=True, exist_ok=True)
-
-ENCODER_FILE = ENCODER_DIR / "onehot_encoder.pkl"
-
-if ENCODER_FILE.exists():
-    encoder = joblib.load(ENCODER_FILE)
-    print("Encoder geladen.")
-else:
-    encoder = OneHotEncoder(
-        categories=[
-            [0, 1, 2, 3, 4, 5, 6],
-            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-        ],
-        sparse_output=False,
-        drop="first",
-        handle_unknown="ignore"
-    )
-
-    encoder_fit_data = pd.DataFrame({
-        "day_of_week": [0, 1, 2, 3, 4, 5, 6] * 12,
-        "month": sorted(list(range(12)) * 7)
-    })
-
-    encoder.fit(encoder_fit_data[ENCODER_COLUMNS])
-
-    joblib.dump(encoder, ENCODER_FILE)
-    print("Encoder erstellt und gespeichert.")
+creating_encoder_scaler.create_encoder()
+creating_encoder_scaler.create_scaler()
 
 # ==========================
 # Chunkweise Verarbeitung
