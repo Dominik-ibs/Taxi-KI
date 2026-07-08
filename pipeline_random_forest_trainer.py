22b1118842b178c12ac6ad6785ff9986a4b9fe7d
00090854 <hannes.luebbers@enercon.de>
2026-07-09 00:19:32 +0200

    Adding Random Forest model with not all data

--- pipeline_random_forest_trainer.py
+++ pipeline_random_forest_trainer.py
@@ -2,25 +2,32 @@ import joblib
 import os
 import pandas as pd
 import sqlite3
-
 from sklearn.ensemble import RandomForestRegressor
 
 SOURCE_DB_PATH = "database/clean_train.sqlite"
 MODEL_PATH = "model/randomForestModel"
+SAMPLE_HASH = 2654435761
+
+# 67 Million rows incur a RAM usage of around 70 GB
+# Use this divider to reduce the RAM usage by randomly sampling
+SAMPLE_DIVIDER = 5
 
 QUERY = f"""
 SELECT *
 FROM tripdata
+WHERE ABS(rowid * {SAMPLE_HASH}) % {SAMPLE_DIVIDER} = 0
 """
 
 source_conn = sqlite3.connect(SOURCE_DB_PATH)
 
 model = RandomForestRegressor(
-    n_estimators=500,
+    n_estimators=300,
     max_depth=20,
-    min_samples_split=5,
-    min_samples_leaf=2,
+    min_samples_split=10,
+    min_samples_leaf=5,
+    max_features="sqrt",
     n_jobs=-1,
+    random_state=69,
     verbose=1
 )
 
@@ -32,14 +39,13 @@ features = features.drop(columns=["Duration"])
 print(features.describe())
 print(targets.describe())
 
-print("Training started...")
+print("Starting training...")
 
 model.fit(features, targets)
 
 print("Training finished")
 
 os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
-
 joblib.dump(model, MODEL_PATH)
 
-print(f"Model saved: {MODEL_PATH}")
+print(f"Model Saved: {MODEL_PATH}")
